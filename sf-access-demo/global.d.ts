d74f77ce5f8284c5446ebb9ccdcb3c0dde308169
matthewru <matthewru2005@gmail.com>
2025-04-06 09:27:08 -0700

    typo

--- sf-access-demo/global.d.ts
+++ sf-access-demo/global.d.ts
@@ -1,7 +1,7 @@
 // global.d.ts
 declare namespace JSX {
   interface IntrinsicElements {
-    'eclec-tech': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+    'eqlec-tech': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
       'api-key'?: string;
     };
   }
